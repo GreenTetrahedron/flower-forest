94086414a1d45fd7058df83be804b856533124de
Hamza Maniar <maniarhamza01@gmail.com>
2023-06-25 23:43:26 +0100

    API get actions are working, need to implement a better return value when records are not found.

---
+++ FlowerForestAPI/FlowerForestAPI/Models/FlowerForestContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlowerForestAPI.Models
+{
+    public class FlowerForestContext : DbContext
+    {
+        public FlowerForestContext(DbContextOptions<FlowerForestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Plant> Plants { get; set; }
+    }
+}
---
+++ FlowerForestAPI/FlowerForestAPI/Models/Plant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowerForestAPI.Models
+{
+    public class Plant
+    {
+        public Guid Id { get; set; }
+
+        public string Genus { get; set; }
+
+        public string Species { get; set; }
+
+        public double MaxHeight_metres { get; set; }
+
+        public string? CommonName { get; set; }
+
+        public string PhotoUrl { get; set; }
+    }
+}
