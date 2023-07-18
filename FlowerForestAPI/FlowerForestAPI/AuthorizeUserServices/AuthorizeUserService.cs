c266b8bc878255cdd16f88507e710de6ecd43370
Hamza Maniar <maniarhamza37@gmail.com>
2023-07-18 22:41:27 +0100

    Added new user authorization service

---
+++ FlowerForestAPI/FlowerForestAPI/AuthorizeUserServices/AuthorizeUserService.cs
@@ -0,0 +1,27 @@
+﻿using FlowerForestAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FlowerForestAPI.AuthorizeUserServices
+{
+    public class AuthorizeUserService : IAuthorizeUserService
+    {
+        private readonly IAuthorizationService authorizationService;
+
+        public AuthorizeUserService(IAuthorizationService authorizationService)
+        {
+            this.authorizationService = authorizationService;
+        }
+
+        public async Task<AuthorizationResult> AuthorizeUserId(ClaimsPrincipal claims, Guid id)
+        {
+            var authorizationResult = await authorizationService.AuthorizeAsync(claims, new User() { Id = id }, "CreatorOnlyPolicy");
+
+            return authorizationResult;
+        }
+    }
+}
---
+++ FlowerForestAPI/FlowerForestAPI/AuthorizeUserServices/IAuthorizeUserService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FlowerForestAPI.AuthorizeUserServices
+{
+    public interface IAuthorizeUserService
+    {
+        Task<AuthorizationResult> AuthorizeUserId(ClaimsPrincipal claims, Guid id);
+    }
+}
